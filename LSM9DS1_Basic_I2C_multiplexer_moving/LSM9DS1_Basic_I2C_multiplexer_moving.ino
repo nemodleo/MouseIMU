0349dd69b2178380cd5288a1e52ec1514b1dc9f7
HYUN PARK <49754293+nemodleo@users.noreply.github.com>
2021-08-28 00:08:47 +0900

    Add files via upload

---
+++ LSM9DS1_Basic_I2C_multiplexer_moving/LSM9DS1_Basic_I2C_multiplexer_moving.ino
@@ -0,0 +1,189 @@
+/*****************************************************************
+LSM9DS1_Basic_I2C.ino
+https://github.com/sparkfun/LSM9DS1_Breakout
+* How to read the gyroscope, accelerometer, and magnetometer
+  using the readGryo(), readAccel(), readMag() functions and 
+  the gx, gy, gz, ax, ay, az, mx, my, and mz variables.
+* How to calculate actual acceleration, rotation speed, 
+  magnetic field strength using the calcAccel(), calcGyro() 
+  and calcMag() functions.
+* How to use the data from the LSM9DS1 to calculate 
+  orientation and heading.
+*****************************************************************/
+#include <Wire.h>
+#include <SPI.h> //
+#include <SparkFunLSM9DS1.h>
+
+LSM9DS1 imu;
+#define LSM9DS1_M 0x1E 
+#define LSM9DS1_AG  0x6B 
+#define TCAADDR 0x70
+
+#define PRINT_CALCULATED
+#define PRINT_SPEED 10//250 
+static unsigned long lastPrint = 0; 
+#define DECLINATION -8.58
+
+
+
+int vx;
+int sx;
+
+
+void tcaselect(uint8_t i) {
+    if (i > 7) 
+        return;
+
+    Wire.beginTransmission(TCAADDR);
+    Wire.write(1 << i);
+    Wire.endTransmission();  
+}
+
+void setup() 
+{ 
+  Serial.begin(115200);
+  Wire.begin();
+  tcaselect(1);
+  
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+
+  imu.settings.accel.scale = 8;
+  if (!imu.begin())
+  {
+    Serial.println("Failed to communicate with LSM9DS1.");
+    Serial.println("Double-check wiring.");
+    Serial.println("Default settings in this sketch will " \
+                  "work for an out of the box LSM9DS1 " \
+                  "Breakout, but may need to be modified " \
+                  "if the board jumpers are.");
+    while (1)
+      ;
+  }
+  vx=0;
+  sx=0;
+}
+
+void loop()
+{
+  tcaselect(1);
+  if ( imu.gyroAvailable() )
+  {
+    imu.readGyro();
+  }
+  if ( imu.accelAvailable() )
+  {
+    imu.readAccel();
+  }
+  if ( imu.magAvailable() )
+  {
+    imu.readMag();
+  }
+  while((lastPrint + PRINT_SPEED) > millis()){}
+  //.print(millis()-lastPrint);Serial.print(" ");
+
+  Serial.print(9.7983*imu.calcAccel(imu.ax));
+  vx += 9.7983*imu.calcAccel(imu.ax);
+  Serial.print(" ");Serial.print(vx);
+
+  /*
+  printGyro();  // Print "G: gx, gy, gz"
+  printAccel(); // Print "A: ax, ay, az"
+  printMag();   // Print "M: mx, my, mz"
+  printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
+  */
+  Serial.println();
+  
+  lastPrint = millis();
+  
+}
+
+
+void printGyro()
+{
+  Serial.print("G: ");
+#ifdef PRINT_CALCULATED
+  Serial.print(imu.calcGyro(imu.gx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gy), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcGyro(imu.gz), 2);
+  Serial.println(" deg/s");
+#elif defined PRINT_RAW
+  Serial.print(imu.gx);
+  Serial.print(", ");
+  Serial.print(imu.gy);
+  Serial.print(", ");
+  Serial.println(imu.gz);
+#endif
+}
+
+void printAccel()
+{  
+  Serial.print("A: ");
+#ifdef PRINT_CALCULATED
+  Serial.print(imu.calcAccel(imu.ax), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.ay), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcAccel(imu.az), 2);
+  Serial.println(" g");
+#elif defined PRINT_RAW 
+  Serial.print(imu.ax);
+  Serial.print(", ");
+  Serial.print(imu.ay);
+  Serial.print(", ");
+  Serial.println(imu.az);
+#endif
+
+}
+
+void printMag()
+{  
+  Serial.print("M: ");
+#ifdef PRINT_CALCULATED
+  Serial.print(imu.calcMag(imu.mx), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.my), 2);
+  Serial.print(", ");
+  Serial.print(imu.calcMag(imu.mz), 2);
+  Serial.println(" gauss");
+#elif defined PRINT_RAW
+  Serial.print(imu.mx);
+  Serial.print(", ");
+  Serial.print(imu.my);
+  Serial.print(", ");
+  Serial.println(imu.mz);
+#endif
+}
+
+void printAttitude(float ax, float ay, float az, float mx, float my, float mz)
+{
+  float roll = atan2(ay, az);
+  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
+  
+  float heading;
+  if (my == 0)
+    heading = (mx < 0) ? PI : 0;
+  else
+    heading = atan2(mx, my);
+    
+  heading -= DECLINATION * PI / 180;
+  
+  if (heading > PI) heading -= (2 * PI);
+  else if (heading < -PI) heading += (2 * PI);
+  else if (heading < 0) heading += 2 * PI;
+  
+  // Convert everything from radians to degrees:
+  heading *= 180.0 / PI;
+  pitch *= 180.0 / PI;
+  roll  *= 180.0 / PI;
+  
+  Serial.print("Pitch, Roll: ");
+  Serial.print(pitch, 2);
+  Serial.print(", ");
+  Serial.println(roll, 2);
+  Serial.print("Heading: "); Serial.println(heading, 2);
+}
+
